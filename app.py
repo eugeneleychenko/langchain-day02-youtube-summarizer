df81178b815904e5017c8517d51eaa9839f819b9
eugeneleychenko <nycspicebo@gmail.com>
2023-06-30 14:20:18 -0400

    final

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from langchain import PromptTemplate, LLMChain
 from langchain.document_loaders import YoutubeLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
@@ -6,22 +7,50 @@ from dotenv import find_dotenv, load_dotenv
 import tiktoken
 import os
 import streamlit as st
+import time
 
-load_dotenv()
+
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 #load YT url
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", openai_api_key=openai_api_key)
-loader = YoutubeLoader.from_youtube_url("https://www.youtube.com/watch?v=uJQmCFTYCh8", add_video_info=True)
-result = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=0)
-texts = text_splitter.split_documents(result)
+def load_yt(url):
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", openai_api_key=openai_api_key)
+    loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
+    result = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=0)
+    texts = text_splitter.split_documents(result)
 
-chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=True)
-chain.run(texts[:4])
+    chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=True)
+    ran_chain = chain.run(texts)
+    return ran_chain
 
 
+def main():
+    load_dotenv(find_dotenv())
+
+    st.set_page_config(page_title="Youtube Summarizer")
+    st.header("Youtube Summarizer")
+    url = st.text_input("Enter the Youtube URL of the Video You Want to Summarize")
+    
+   
+    if url:
+            
+            st.write("Summarizing this video: ", url)
+            
+            with st.spinner('Transcribing video'):
+                time.sleep(15)
+            #functions
+            transcript = load_yt(url)
+            with st.spinner('Summarizing video'):
+                time.sleep(10)
+            # bullets = generate_bullets(transcript)
+            
+           
+            # st.success('Done!')
+        
+            with st.expander("Summary"):
+                st.write(transcript)
 
 
 
